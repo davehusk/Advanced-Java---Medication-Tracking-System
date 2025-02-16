8fa5c27c7344fafc9530e9794f1f752297709375
Dave Husk <145527357+davehusk@users.noreply.github.com>
2025-02-16 18:21:07 -0330

    Fixed Medication expiry logic & added search

--- Medication.java
+++ Medication.java
@@ -1,16 +1,30 @@
 package medicationtracking;
 
-// This is the medication class. It stores drug info.
+import java.time.LocalDate;
+import java.util.Random;
+
+// Now it has expiry dates.
 public class Medication {
     private int id;
     private String name;
     private int dosage;
     private int quantityInStock;
+    private LocalDate expiryDate;
 
     public Medication(int id, String name, int dosage, int quantityInStock) {
         this.id = id;
         this.name = name;
         this.dosage = dosage;
         this.quantityInStock = quantityInStock;
+        this.expiryDate = generateRandomExpiry();
+    }
+
+    private LocalDate generateRandomExpiry() {
+        Random random = new Random();
+        return LocalDate.now().plusYears(random.nextInt(4) - 2);
+    }
+
+    public boolean isExpired() {
+        return expiryDate.isBefore(LocalDate.now());
     }
 }
